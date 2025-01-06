4f6a51fd17f7094339ee93d93b7d412a0087b240
Pérola Carvalho <160730999+Perola16@users.noreply.github.com>
2025-01-06 11:29:30 -0300

    Add files via upload

---
+++ ProjetoPython/Teste 01.py
@@ -0,0 +1,25 @@
+print("Olá, mundo, testando o python!")
+print("")
+print("")
+Nome="Maria"
+Idade=25
+Altura=1.68
+Esta_estudando= True
+print("Nome: ",Nome,", Idade: ",Idade,", Altura: ",Altura,", Está Estudando: ",Esta_estudando)
+print("")
+print("")
+nome=input("Digite seu nome: ")
+print("Olá, ",nome," seja bem-vindo!")
+print("")
+print("")
+N1=float(input("Digite o primeiro número: "))
+N2=float(input("Digite o segundo número: "))
+soma=N1+N2
+print("A soma é: ",soma)
+print("")
+print("")
+numero=int(input("Digite um número: "))
+if(numero%2==0):
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
